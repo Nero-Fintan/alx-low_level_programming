12c3d7a937b4a5acd27395f0af3557e62a7f0400
Nero-Fintan <traderdera@gmail.com>
2023-03-22 22:00:53 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - ads two integers and returns the result
+ * Oa: int to be added to a
+ * Ob: int to be added to b
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
