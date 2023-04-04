b2f16bd59e69b8be2524cfeb5a6481cabe3f56f1
Nero-Fintan <traderdera@gmail.com>
2023-04-04 16:45:36 +0000

    lower alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,15 @@
 
 /**
  * print_alphabet - print all alphabets in lowercase
- */
-
+ * Description: : 'print alphabets lowercase'
+ * Return: Always (0)
+*/
 void print_alphabet(void)
 {
-        char letter;
+	char letter;
 
-        for (letter = 'a'; letter <= 'z'; letter++)
-                _putchar(letter);
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
 
-        _putchar('\n');
-
+	_putchar('\n');
+}
